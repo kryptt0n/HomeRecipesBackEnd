1310e66cb21511297442360b2775d17cdab40066
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-06-27 22:52:57 -0400

    removed unused imports

--- src/main/java/com/vitalysukhinin/homerecipes/controllers/ProductController.java
+++ src/main/java/com/vitalysukhinin/homerecipes/controllers/ProductController.java
@@ -4,7 +4,6 @@ import com.vitalysukhinin.homerecipes.entities.Product;
 import com.vitalysukhinin.homerecipes.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
